104e8de726f7ef1eba5bd99779f21fdc20b27735
Paul McGeachie <paulmcgeachie@icloud.com>
2021-12-13 08:20:38 +0000

    finish assignment

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = Vue.createApp({
     return {
       name: "Paul",
       age: 43,
+      addFive: 5,
+      myLink:
+        "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989__340.jpg",
     };
+  },
+  methods: {
+    myNewAge() {
+      return (newAge = this.age + this.addFive);
+    },
+    randomNum() {
+      return Math.random();
+    },
   },
 });
 
